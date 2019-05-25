8b93819ffa3e288573cb679d9b1d5b9037f5fdcf
NitroTaker <nittrotaker@yahoo.com>
2019-05-25 12:39:44 -0700

    Add Physics

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+    private Vector3 velocity;
+	private void Start ()
+    {
+        float x = Random.Range(-2.0f, 2.0f);
+        float z = Random.Range(-2.0f, 2.0f);
+        velocity = new Vector3(x, 0, z);
+
+    }
+    private void Update()
+    {
+        transform.Translate(velocity);
+    }
+}
