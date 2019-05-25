103bb7e30c49f49c7bcba71043959f32d4c776a5
NitroTaker <nittrotaker@yahoo.com>
2019-05-25 11:57:34 -0700

    Create Input and Movement Script for Player

---
+++ Assets/Scripts/InputDetectorAndMover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class InputDetectorAndMover : MonoBehaviour {
+    public float speed = 0.2f;
+
+    private void Update()
+    {
+        float velocity = Input.GetAxis("Vertical") * speed;
+        gameObject.transform.Translate(Vector3.forward * velocity);
+
+    }
+}
