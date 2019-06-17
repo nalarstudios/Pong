5ee6ced2b5aad6aad246ef0dd917324a489d7a7e
NitroTaker <nittrotaker@yahoo.com>
2019-06-17 14:52:54 -0700

    Pauser

---
+++ Assets/Scripts/Pauser.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Pauser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+}
