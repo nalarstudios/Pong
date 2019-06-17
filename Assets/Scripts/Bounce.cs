295bcd36ffd98c234bfd5ae9f0485e690c3a4e7a
NitroTaker <nittrotaker@yahoo.com>
2019-06-16 17:19:33 -0700

    Add Collisions

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -2,15 +2,24 @@
 
 public class Bounce : MonoBehaviour {
     private Vector3 velocity;
+    Rigidbody rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 	private void Start ()
     {
         float x = Random.Range(-2.0f, 2.0f);
         float z = Random.Range(-2.0f, 2.0f);
-        velocity = new Vector3(x, 0, z);
-
+        velocity = new Vector3(5.0f, 0, 5.9f);
+        rb.velocity = velocity;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("We Hit Something");
     }
     private void Update()
     {
-        transform.Translate(velocity);
+        //transform.Translate(velocity);
     }
 }
