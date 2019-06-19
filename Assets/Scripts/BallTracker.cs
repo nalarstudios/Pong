2c19d46fd78dcd969b269c727272a93e7ccd863c
NitroTaker <nittrotaker@yahoo.com>
2019-06-18 17:28:04 -0700

    Score Detection

---
+++ Assets/Scripts/BallTracker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BallTracker : MonoBehaviour {
+    public GameObject ball;
+    public GameObject playerScore;
+	// Use this for initialization
+	void Start () {
+        playerScore.GetComponent<Text>().text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(ball.transform.position.x > 13)
+        {
+            Text text = playerScore.GetComponent<Text>();
+            int score = int.Parse(text.text);
+            score++;
+            text.text = score.ToString();
+        }
+	}
+}
