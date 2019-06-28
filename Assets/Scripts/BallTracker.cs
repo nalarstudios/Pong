5fb1b6629f77922eff89801cd440a836570e93c1
NitroTaker <nittrotaker@yahoo.com>
2019-06-27 18:04:05 -0700

    Playing Sound When AI Scores

--- Assets/Scripts/BallTracker.cs
+++ Assets/Scripts/BallTracker.cs
@@ -4,23 +4,39 @@ using UnityEngine.UI;
 public class BallTracker : MonoBehaviour {
     public GameObject ball;
     public GameObject playerScore;
-    public AudioSource winSound;
+    public GameObject aiScore;
+    private AudioSource scoreSound;
+    public AudioClip loseClip;
+    public AudioClip winClip;
 	// Use this for initialization
 	void Start () {
         playerScore.GetComponent<Text>().text = "0";
-        winSound = GetComponent<AudioSource>();
+        scoreSound = GetComponent<AudioSource>();
+        aiScore.GetComponent<Text>().text = "0";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(ball.transform.position.x > 13)
+        if (ball.transform.position.x > 13)
         {
             Text text = playerScore.GetComponent<Text>();
             int score = int.Parse(text.text);
             score++;
             text.text = score.ToString();
             ball.transform.position = Vector3.zero;
-            winSound.Play();
+            scoreSound.clip = winClip;
+            scoreSound.Play();
         }
+        else if (ball.transform.position.x < -13)
+        {
+            Text text = aiScore.GetComponent<Text>();
+            int score = int.Parse(text.text);
+            score++;
+            text.text = score.ToString();
+            ball.transform.position = Vector3.zero;
+            scoreSound.clip = loseClip;
+            scoreSound.Play();
+        }
+
 	}
 }
