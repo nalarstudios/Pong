14f13cc6317e1100f47f751cde77385da82e11ae
NitroTaker <nittrotaker@yahoo.com>
2019-07-08 05:10:33 -0700

    Collision Sounds

---
+++ Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour {
+
+    public AudioSource tickSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tickSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Collidable")
+        {
+            tickSource.Play();
+        }
+    }
+}
