56bec91dfbe1a32a6750b3389ad7f02bd4008360
NitroTaker <nittrotaker@yahoo.com>
2019-07-10 11:19:07 -0700

    Return to Main Menu

---
+++ Assets/Scripts/Return.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Return : MonoBehaviour
+{
+    private SceneChanger changer;
+	// Use this for initialization
+	void Start ()
+    {
+        changer = GetComponent<SceneChanger>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            changer.ChangeScene(-1);
+            print("Deathhh");
+        }
+    }
+}
--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class SceneChanger : MonoBehaviour {
 
-	public void PlayGame ()
+	public void ChangeScene (int index)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + index);
         Debug.Log("Made It");
     }
 
